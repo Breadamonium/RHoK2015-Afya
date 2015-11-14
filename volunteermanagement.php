4498ec19a6561ac222b6251fcdf14136ec6ff441
Jeff Ouellette <jco65@cornell.edu>
2015-11-14 13:54:25 -0500

    Fixed an issue

--- volunteermanagement.php
+++ volunteermanagement.php
@@ -8,7 +8,7 @@ class volunteermanagementsystem extends SQLite3 {
         $this->open($databasefile)
     }
 
-    function add_volunteer($name, $username, $address, $phone, $email, $remoteaccessallowed) {
+    function add_volunteer($name, $username, $address, $phone, $email, $remoteaccessallowed, $under18) {
         $this->exec("INSERT INTO volunteers (name, username, address, phone, email, hours, remoteaccessallowed, under18, signedin) VALUES ($name, $username, $address, $phone, $email, $remoteaccessallowed, $under18, False)"
     }
 }

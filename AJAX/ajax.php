651c9a3d3352510193389afb6adfa99000b75bd9
al744 <al744@cornell.edu>
2015-11-14 21:03:43 -0500

    added ajax stuff

---
+++ AJAX/ajax.php
@@ -0,0 +1,18 @@
+<?php
+
+if(!empty($_POST['name'])) {
+	$name = $_POST['name'];
+	if($name == 'username') {
+		/*try {
+			$db = new volunteermanagementsystem;
+		} catch {
+			echo "Cannot connect to database."
+		}*/
+		echo true;
+	}
+	if($name == 'register-form') {
+		echo true;
+	}
+}
+
+?>
